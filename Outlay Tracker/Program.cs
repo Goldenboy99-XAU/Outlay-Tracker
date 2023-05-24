229ba3a20ca4d07cd1bdd2cfbffa074dfc84f5a6
Goldenboy99-XAU <110536745+Goldenboy99-XAU@users.noreply.github.com>
2023-05-24 15:00:12 +0200

    Final Commit

--- Outlay Tracker/Program.cs
+++ Outlay Tracker/Program.cs
@@ -16,7 +16,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -28,3 +32,4 @@ app.MapControllerRoute(
     pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
+
